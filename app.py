f9f49251d2b9a8cc58aa28bee9e9446d9e18eead
annv0-0 <haianvo@usf.edu>
2025-11-19 14:16:31 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ hours = float(hours)
 weekly_hours = hours * 7 
 
 print(f"You are on track to study {weekly_hours} hours this week.")
+
+
+try:
+    hours = float(hours)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+
